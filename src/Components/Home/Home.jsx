5d99cd5c0a748cd87a132490b649e307d526a0b3
DavidNabil210 <davidnabil633@gmail.com>
2025-05-23 23:56:30 +0300

    message

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -21,19 +21,25 @@ export default function Home() {
     //   <p className='bg-red-700 w-6 text-3xl'>{Counter}</p>
     //   <button onClick={ChangeCounter}>ok</button>
     // </div>
-    <div className="grid lg:grid-cols-4 sm:grid-cols-1">
-      {
-        Products.map((product)=>(
-          <div key={product.id}>
-              <img src={product.imageCover} alt="imageCover" />
-              <p className='text-indigo-500 text-center '>{product.title}</p>
-              <p>{product.price}</p>
-          </div>
-        )
-
-        )
-      }
+ <div className="grid lg:grid-cols-6 sm:grid-cols-1 gap-6 p-4">
+  {Products.map((product) => (
+    <div
+      key={product.id}
+      className="bg-white rounded-lg shadow-md p-4 flex flex-col items-center h-full"
+    >
+      <div className="w-full h-48 flex items-center justify-center">
+        <img
+          src={product.imageCover}
+          alt="imageCover"
+          className="max-h-full object-contain"
+        />
+      </div>
+      <p className="text-indigo-500 text-center mt-4">{product.title.split(" ").slice(0, 2).join(" ")}</p>
+      <p className="text-center font-semibold mt-auto">{product.price} <span className='text-indigo-700'> $</span> </p>
     </div>
+  ))}
+</div>
+
     
   )
 }
