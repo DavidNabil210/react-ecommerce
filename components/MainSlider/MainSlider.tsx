5feb5f8af9c7d3c28344c51be0fc95ea8c6db70a
DavidNabil210 <davidnabil633@gmail.com>
2025-05-30 18:25:59 +0300

    added Home Components

---
+++ components/MainSlider/MainSlider.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+
+
+interface MainSliderProps {}
+
+const MainSlider: FC<MainSliderProps> = () => (
+  <div>
+    MainSlider Component
+  </div>
+);
+
+export default MainSlider;
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { CounterContext } from '../../Context/CounterContext'
 import axios from 'axios';
+import MainSlider from '../../../components/MainSlider/MainSlider';
 export default function Home() {
   const{Counter,ChangeCounter}=useContext(CounterContext);
   const [Products, setProducts] = useState([]);
@@ -17,29 +18,26 @@ export default function Home() {
   
   return (
 
-    // <div >
-    //   <p className='bg-red-700 w-6 text-3xl'>{Counter}</p>
-    //   <button onClick={ChangeCounter}>ok</button>
-    // </div>
- <div className="grid lg:grid-cols-6 sm:grid-cols-1 gap-6 p-4">
-  {Products.map((product) => (
-    <div
-      key={product.id}
-      className="bg-white rounded-lg shadow-md p-4 flex flex-col items-center h-full"
-    >
-      <div className="w-full h-48 flex items-center justify-center">
-        <img
-          src={product.imageCover}
-          alt="imageCover"
-          className="max-h-full object-contain"
-        />
-      </div>
-      <p className="text-indigo-500 text-center mt-4">{product.title.split(" ").slice(0, 2).join(" ")}</p>
-      <p className="text-center font-semibold mt-auto">{product.price} <span className='text-indigo-700'> $</span> </p>
-    </div>
-  ))}
-</div>
-
+   
+//  <div className="grid lg:grid-cols-6 sm:grid-cols-1 gap-6 p-4">
+//   {Products.map((product) => (
+//     <div
+//       key={product.id}
+//       className="bg-white rounded-lg shadow-md p-4 flex flex-col items-center h-full"
+//     >
+//       <div className="w-full h-48 flex items-center justify-center">
+//         <img
+//           src={product.imageCover}
+//           alt="imageCover"
+//           className="max-h-full object-contain"
+//         />
+//       </div>
+//       <p className="text-indigo-500 text-center mt-4">{product.title.split(" ").slice(0, 2).join(" ")}</p>
+//       <p className="text-center font-semibold mt-auto">{product.price} <span className='text-indigo-700'> $</span> </p>
+//     </div>
+//   ))}
+// </div>
+  <MainSlider/>
     
   )
 }
