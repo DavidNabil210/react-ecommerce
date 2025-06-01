fbe8bec234be377fce6d5ead08847206d3cbe288
DavidNabil210 <davidnabil633@gmail.com>
2025-06-01 03:28:37 +0300

    Updated ProductDetails Component

--- src/Components/ProductDetails/ProductDetails.jsx
+++ src/Components/ProductDetails/ProductDetails.jsx
@@ -6,8 +6,9 @@ export default function ProductDetails() {
     const{id}=useParams();
     const [Product, setProduct] = useState(null)
     async function GetProduct() {
-         const {data}= await axios.get(`https://ecommerce.routemisr.com/api/v1/products${id}`)
+         const {data}= await axios.get(`https://ecommerce.routemisr.com/api/v1/products/${id}`)
          console.log(data.data);
+         setProduct(data.data);
     }
     useEffect(() => {
       GetProduct();
@@ -16,7 +17,32 @@ export default function ProductDetails() {
     }, [])
     
   return (
-    <div>ProductDetails</div>
-  )
+  <>
+    <h1 className="text-2xl font-bold mb-4 text-center">Product Details</h1>
+
+    {Product ? (
+      <div className="grid grid-cols-1 md:grid-cols-12 gap-6 p-4 items-start">
+      
+        <div className="md:col-span-4">
+          <img
+            src={Product.imageCover}
+            alt={Product.title}
+            className="w-full h-auto rounded-lg shadow-md"
+          />
+        </div>
+
+       
+        <div className="md:col-span-8 space-y-4">
+          <h2 className="text-xl font-semibold">{Product.title}</h2>
+          <p className="text-gray-700">{Product.description}</p>
+          <p className="text-lg font-bold text-green-600">Price: {Product.price} EGP</p>
+        </div>
+      </div>
+    ) : (
+      <p className="text-center text-gray-500">Loading...</p>
+    )}
+  </>
+);
+
 }
 
