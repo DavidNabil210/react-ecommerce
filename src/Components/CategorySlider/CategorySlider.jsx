6a51a48c4632cf0f4a70807d03ae5c64873d48d0
DavidNabil210 <davidnabil633@gmail.com>
2025-06-06 19:47:14 +0300

    message

--- src/Components/CategorySlider/CategorySlider.jsx
+++ src/Components/CategorySlider/CategorySlider.jsx
@@ -3,19 +3,33 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import LoadingSpinner from "../Loadingspinner/LoadingSpinner";
 
 export default function CategorySlider() {
-  const [Categories, setCategories] = useState([]);
-  async function GetCategories() {
-    const {data}=await axios.get("https://ecommerce.routemisr.com/api/v1/categories")
-    // console.log(data.data);
-    setCategories(data.data);
-  }
-  useEffect(() => {
-    GetCategories();
+  // const [Categories, setCategories] = useState([]);
+  // async function GetCategories() {
+  //   const {data}=await axios.get("https://ecommerce.routemisr.com/api/v1/categories")
+  //   // console.log(data.data);
+  //   setCategories(data.data);
+  // }
+  // useEffect(() => {
+  //   GetCategories();
   
     
-  }, [])
+  // }, [])
+  const {data,isError,error,isLoading}= useQuery({
+    queryKey: ['categories'],
+    queryFn: ()=> axios.get("https://ecommerce.routemisr.com/api/v1/categories"),
+
+  })
+  const categories=data?.data?.data;
+  if(isLoading){
+    return <LoadingSpinner/>
+  }
+  if(isError){
+    console.log(error.message);
+  }
   
     const settings = {
     dots: false,
@@ -46,8 +60,8 @@ export default function CategorySlider() {
   return (
    <div className="container mx-auto px-4 py-6">
     <h2 className="text-xl font-bold mb-4 text-indigo-600">Shop Popular Categories</h2>
-    {Categories && Categories.length > 0 ?( <Slider {...settings}>
-      {Categories.map((category)=>(
+    <Slider {...settings}>
+      {categories.map((category)=>(
         <div key={category._id}>
           <div className="bg-white overflow-hidden shadow-md rounded-lg text-center">
             <img src={category.image} alt={category.name} className="object-cover h-40 w-full" />
@@ -57,7 +71,7 @@ export default function CategorySlider() {
       ))
 
       }
-    </Slider>):( <p className="text-center text-gray-500">Loading...</p>)}
+    </Slider>
    </div>
   );
 }
